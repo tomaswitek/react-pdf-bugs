692dd822930b5fce17accd78df70af60d7ad0b2e
Tomáš Witek <tomas@witek.cz>
2020-07-16 09:02:16 +0200

    render to file

--- pages/pdf-without-graphql.tsx
+++ pages/pdf-without-graphql.tsx
@@ -1,16 +1,23 @@
-import ReactPdf from "@react-pdf/renderer";
+import ReactPDF, {pdf} from "@react-pdf/renderer";
 import RatesPDF from "../components/RatesPDF";
 
 const PdfPage = () => null;
 
 PdfPage.getInitialProps = async ({res}) => {
-  const stream = await ReactPdf.renderToStream(<RatesPDF currency="USD" />);
+  // const stream = await ReactPdf.renderToStream(<RatesPDF currency="USD" />);
 
-  if (stream && res) {
-    res.setHeader("Content-disposition", 'attachment; filename="rates.pdf"');
-    res.setHeader("Content-Type", "application/pdf");
-    stream.pipe(res);
-  }
+  await ReactPDF.render(<RatesPDF currency="USD" />, `output.pdf`);
+  // const stream = await pdf(<RatesPDF currency="USD" />);
+
+  // console.log(stream);
+
+  // const string = await stream.toBuffer();
+
+  // if (stream && res) {
+  //   // res.setHeader("Content-disposition", 'attachment; filename="rates.pdf"');
+  //   // res.setHeader("Content-Type", "application/pdf");
+  //   stream.pipe(stream);
+  // }
 
   return {};
 };
