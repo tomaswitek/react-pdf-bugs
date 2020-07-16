20005eadf0cc6484f581c4009a80570fc0dadbc2
Tomáš Witek <tomas@witek.cz>
2020-07-16 12:47:05 +0200

    waitfor api

--- pages/api/pdf.tsx
+++ pages/api/pdf.tsx
@@ -1,7 +1,7 @@
 import ReactPDF from "@react-pdf/renderer";
 import {NextApiRequest, NextApiResponse} from "next";
 import PdfPage from "../pdf";
-// import {getMarkupFromTree} from "@apollo/client/react/ssr";
+import {getMarkupFromTree} from "@apollo/client/react/ssr";
 
 export const config = {
   api: {
@@ -12,13 +12,13 @@ export const config = {
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader("Content-Type", "application/pdf");
   // This renders to a stream but doesn't wait for the API calls to be finshed
-  const stream = await ReactPDF.renderToStream(<PdfPage currency="USD" />);
+  // const stream = await ReactPDF.renderToStream(<PdfPage currency="USD" />);
 
   // This waits for API calls but expects to render to a string
-  // const stream = await getMarkupFromTree({
-  //   tree: <PdfPage currency="USD" />,
-  //   renderFunction: ReactPDF.renderToStream,
-  // });
+  const stream = await getMarkupFromTree({
+    tree: <PdfPage currency="USD" />,
+    renderFunction: ReactPDF.renderToStream,
+  });
 
   // Sends data back as a response
   stream.on("data", (data) => {
